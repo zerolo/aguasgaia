b17aa1fde27a8859f9ec853a75e35d9fcc181733
zerolo <ze.p.rolo@gmail.com>
2024-05-22 23:22:32 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="aguasgaia",
-    version="0.0.17",
+    version="0.0.18",
     python_requires='>=3',
     package_dir={'': '.'},
     packages=find_namespace_packages(where='.'),
