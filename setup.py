a4e3e21c81bbd12b181349d4cb88ee2b6eec8300
zerolo <ze.p.rolo@gmail.com>
2024-05-27 12:26:02 +0100

    small adjustments

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="aguasgaia",
-    version="0.0.18",
+    version="0.0.19",
     python_requires='>=3',
     package_dir={'': '.'},
     packages=find_namespace_packages(where='.'),
